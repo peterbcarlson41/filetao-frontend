769f902b51c15c9b304df48931ad64e6aef00f61
peterbcarlson41 <75817779+peterbcarlson41@users.noreply.github.com>
2024-04-11 23:05:34 -0400

    Added changes for mobile on landing page.

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ function App() {
       <div className="flex items-center justify-center h-screen">
         <h1 className="flex flex-row items-center sm:text-9xl text-7xl font-bold text-center text-gray-800 drop-shadow-[0_5px_3px_rgba(0,0,0,0.7)]">
           File
-          <Tao className="h-24 w-24" />
+          <Tao className="sm:h-24 sm:w-24 h-14 w-14" />
         </h1>
       </div>
     </>
